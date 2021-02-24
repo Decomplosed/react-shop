8f23768499eac42949534b1d58cd930c73e8070d
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-24 23:01:57 +0100

    created hardcoded data

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Grid from '@material-ui/core';
 
+const products = [
+  { id: 1, name: 'Shoes', description: 'Running shoes.' },
+  { id: 2, name: 'MacBook', description: 'Apple MacBook.' },
+];
+
 const Products = () => {
   return (
     <main>
