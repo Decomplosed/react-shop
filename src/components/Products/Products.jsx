b65ccdfc7d57e04fd7bf000c551956a0e9ce26cd
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-24 23:00:17 +0100

    render Grid

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Grid from '@material-ui/core';
 
 const Products = () => {
-  return <main></main>;
+  return (
+    <main>
+      <Grid></Grid>
+    </main>
+  );
 };
 
 export default Products;
