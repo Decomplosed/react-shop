aa4faa03169c5e31b4146241b1880814f582324e
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-25 16:07:49 +0100

    added responsivity

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -11,7 +11,7 @@ const Products = () => {
     <main>
       <Grid container justify='center' spacing={4}>
         {products.map((product) => (
-          <Grid item key={product.id}></Grid>
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}></Grid>
         ))}
       </Grid>
     </main>
