3ac8c60932e29448ebaf49d94fa0a7d0c1a11d66
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-25 16:10:24 +0100

    imported component from material ui

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
 
 const Product = () => {
   return <div></div>;
