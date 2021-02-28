b7f3e75c895b2218c43e1f3f639d0bce7b2fbf42
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-28 23:02:47 +0100

    styled appBar

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,3 +1,12 @@
 import { makeStyles, fade } from '@material-ui/core/styles';
 
-export default makeStyles((theme) => ({})
+export default makeStyles((theme) => ({
+  appBar: {
+    boxShadow: 'none',
+    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+})
