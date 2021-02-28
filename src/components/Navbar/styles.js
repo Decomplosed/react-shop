3464ffeafc7a4e0c284c25168b3c2a6afcca210a
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-28 23:05:07 +0100

    styled searchIcon

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -43,4 +43,13 @@ export default makeStyles((theme) => ({
       width: 'auto',
     },
   },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 })
