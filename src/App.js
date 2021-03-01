254d636531870a610620c7ad033377ba485f8086
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-01 14:42:48 +0100

    useState case for products variable

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { commerce } from './lib/commerce';
 import { Products, Navbar } from './components';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
   return (
     <div>
       <Navbar />
