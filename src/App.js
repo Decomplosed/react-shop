6c9a6819ceeb834213eb76c9f9e52b8fce66d270
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-01 14:42:59 +0100

    imported useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 
 import { Products, Navbar } from './components';
