0ed982e00bdfc9ba9ad8e38ff7a3c601209a2ad8
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-02 08:14:47 +0100

    passed right path to img source

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -17,7 +17,7 @@ const Product = ({ product }) => {
     <Card className={classes.root}>
       <CardMedia
         className={classes.media}
-        image={product.image}
+        image={product.media.source}
         title={product.name}
       />
       <CardContent>
