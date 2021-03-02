c58a9937f284fbb9dd0ece8f884c95792368ef95
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-02 08:31:55 +0100

    render Container

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 const Cart = () => {
-  return <div></div>;
+  return <Container></Container>;
 };
 
 export default Cart;
