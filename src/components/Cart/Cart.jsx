a2ef1ebd6e238184ae6bc8713aa82047c63248ce
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-02 08:32:33 +0100

    added toolbar container

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 const Cart = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+    </Container>
+  );
 };
 
 export default Cart;
