6378076367b2493fa5fe77e548c7fad4293b63d9
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-02 08:24:41 +0100

    extract onAddToCart

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,7 +4,7 @@ import Product from './Product/Product';
 
 import useStyles from './styles';
 
-const Products = ({ products }) => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
 
   return (
