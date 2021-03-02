a62c6e2d14defaf7eb38767b151dad3aa8596f9b
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-02 08:23:09 +0100

    changed res to item

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   };
 
   const handleAddToCart = async (productId, quantity) => {
-    const res = await commerce.cart.add(productId, quantity);
+    const item = await commerce.cart.add(productId, quantity);
   };
 
   useEffect(() => {
