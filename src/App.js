3711667b6b44d1a82ee94f84d3f66f5ff51124e2
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-03 13:04:57 +0100

    imported Cart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
 const App = () => {
   const [products, setProducts] = useState([]);
