41a408ec89590ab1c0b3df133d7fe165c63ec463
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-04 19:09:45 +0100

    formatting fix

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Typography, Button Card, CardActions, CardContent, CardMedia} from '@material-ui/core'
+import {
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+} from '@material-ui/core';
 
 const CartItem = () => {
   return <div></div>;
