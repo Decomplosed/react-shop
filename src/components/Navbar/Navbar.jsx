f6cd79fbbb57b1dc344bf4a3ed8fa28eddf1cfc9
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-05 22:35:49 +0100

    passed component of Link to IconButton

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,8 +32,11 @@ const Navbar = ({ totalItems }) => {
           </Typography>
           <div className={classes.grow} />
           <div className={classes.button}>
-            <Link to='/cart'></Link>
-            <IconButton aria-label='Show cart items' color='inherit'>
+            <IconButton
+              component={Link}
+              aria-label='Show cart items'
+              color='inherit'
+            >
               <Badge badgeContent={totalItems} color='secondary'>
                 <ShoppingCart />
               </Badge>
