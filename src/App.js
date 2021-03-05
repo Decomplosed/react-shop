382cbcb7015c67b429e37b94b6fb05a1ce7db66f
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-05 22:12:24 +0100

    render Route

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
           <Route exact path='/'>
             <Products products={products} onAddToCart={handleAddToCart} />
           </Route>
-
+          <Route></Route>
           <Cart cart={cart} />
         </Switch>
       </div>
