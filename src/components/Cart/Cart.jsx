165b81140483428a86fa016fcb0281f0fdf28169
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-06 09:08:04 +0100

    imported Link

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import CartItem from './CartItem/CartItem';
+import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 
