2a5dab459f85a6cf645545a94634a59f815703a8
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-07 11:39:17 +0100

    styled layout

--- src/components/CheckoutForm/Checkout/styles.js
+++ src/components/CheckoutForm/Checkout/styles.js
@@ -5,4 +5,15 @@ export default makeStyles((theme) => ({
     position: 'relative',
   },
   toolbar: theme.mixins.toolbar,
+  layout: {
+    marginTop: '5%',
+    width: 'auto',
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
 }));
