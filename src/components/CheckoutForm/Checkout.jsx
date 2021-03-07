cad9346f83db750ed0e43eb8a2288b19cdec31c4
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-07 11:34:27 +0100

    imported components from material UI

--- src/components/CheckoutForm/Checkout.jsx
+++ src/components/CheckoutForm/Checkout.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from '@material-ui/core';
 
 const Checkout = () => {
   return <div></div>;
