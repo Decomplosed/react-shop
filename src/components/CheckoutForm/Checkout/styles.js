4e87936fcf926fa3f9c18f7ab0b17aa7617cff57
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-07 11:39:26 +0100

    styled paper

--- src/components/CheckoutForm/Checkout/styles.js
+++ src/components/CheckoutForm/Checkout/styles.js
@@ -16,4 +16,18 @@ export default makeStyles((theme) => ({
       marginRight: 'auto',
     },
   },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+      marginTop: 60,
+    },
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
 }));
