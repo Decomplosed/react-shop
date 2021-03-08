54099ed03934aacace97522c904c89ad63d66643
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-08 18:24:59 +0100

    render Grid

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -3,7 +3,7 @@ import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form';
 
 const CustomTextField = () => {
-  return <div></div>;
+  return <Grid item xs={12} sm={6}></Grid>;
 };
 
 export default CustomTextField;
