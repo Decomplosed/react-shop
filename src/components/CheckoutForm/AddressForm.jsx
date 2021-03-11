82e56c3bba545662d57a1fc006a5166771315875
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-11 15:25:52 +0100

    render Button

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -141,6 +141,7 @@ const AddressForm = ({ checkoutToken }) => {
             <Button component={Link} variant='outlined' to='/cart'>
               Back to Cart
             </Button>
+            <Button></Button>
           </div>
         </form>
       </FormProvider>
