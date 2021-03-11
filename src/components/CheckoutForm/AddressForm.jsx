e2ede206f22428f776b65d1a8703f7518a302d1b
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-11 15:25:29 +0100

    set variant and to props

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -138,7 +138,7 @@ const AddressForm = ({ checkoutToken }) => {
           </Grid>
           <br />
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Button component={Link}></Button>
+            <Button component={Link} variant='outlined' to='/cart'></Button>
           </div>
         </form>
       </FormProvider>
