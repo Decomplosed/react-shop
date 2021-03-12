4ba851c2bcb36a7221a9e163677cd99b80936597
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-12 07:44:27 +0100

    declared stripePromise

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -7,6 +7,8 @@ import {
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
+const stripePromise = loadStripe();
+
 const PaymentForm = () => {
   return <div></div>;
 };
