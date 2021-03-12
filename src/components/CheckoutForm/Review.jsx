0762981f382288d367083b94f014c1a44010d09c
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-12 07:47:23 +0100

    extracted checkoutToken

--- src/components/CheckoutForm/Review.jsx
+++ src/components/CheckoutForm/Review.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
 
-const Review = () => {
+const Review = ({ checkoutToken }) => {
   return <div></div>;
 };
 
