0b28f5e67159b22a8e4592d19cf20dc7be54a7bf
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-12 07:46:05 +0100

    imported Review

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -7,6 +7,8 @@ import {
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
+import Review from './Review';
+
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const PaymentForm = () => {
