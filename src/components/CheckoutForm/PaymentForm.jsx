d23503e5bcefc8da1382547b1317701ea5120914
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-12 07:43:27 +0100

    imported components related to stripe

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Typography, Button, Divider } from '@material-ui/core';
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from '@stripe/react-stripe-js';
 
 const PaymentForm = () => {
   return <div></div>;
