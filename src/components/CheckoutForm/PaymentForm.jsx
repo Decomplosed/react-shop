bba48b7cd034ed037a55ae80dddef274a42cdefd
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-14 17:52:53 +0100

    passed variant of contained

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -28,7 +28,7 @@ const PaymentForm = ({ checkoutToken }) => {
               <br />
               <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Button variant='outlined'>Back</Button>
-                <Button type='submit'></Button>
+                <Button type='submit' variant='contained'></Button>
               </div>
             </form>
           )}
