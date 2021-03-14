247b1778079bc246748836c64e560290de3f0330
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-14 18:10:00 +0100

    extracted shippingData

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -11,7 +11,7 @@ import Review from './Review';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
-const PaymentForm = ({ checkoutToken, backStep }) => {
+const PaymentForm = ({ checkoutToken, backStep, shippingData }) => {
   const handleSubmit = async (event, elements, stripe) => {
     event.preventDefault();
 
